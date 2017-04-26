83e402a7f161442e2d8ec832c5ab7674ff8064a8
Samuzzal Choudhury <samuzzal@redhat.com>
2017-04-26 14:52:30 +0530

    Rearranged the auth token generation code and added a readme

---
+++ hack/auth_test_fixtures/README.md
@@ -0,0 +1,8 @@
+
+#### How to generate token:
+
+```
+python3 ./generate_auth_token.py
+```
+
+The token will be printed in the standard output which can then be copied and used
---
+++ hack/auth_test_fixtures/generate_auth_token.py
@@ -0,0 +1,37 @@
+'''
+Script to generate an authentication token to be passed as header
+with requests to Bayesian API
+'''
+
+import jwt
+import datetime
+import base64
+
+expiry = datetime.datetime.utcnow() + datetime.timedelta(days=90)
+userid = "testuser"
+f1 = open('./private_key.pem', 'r')
+bayesian_private_key = f1.read()
+token = ""
+
+try:
+    payload = {
+        'exp': expiry,
+        'iat': datetime.datetime.utcnow(),
+        'sub': userid
+    }
+    token = jwt.encode(payload, key=bayesian_private_key, algorithm='RS256')
+    print (token.decode('utf-8'))
+except Exception as e:
+    print(e)
+
+# Following lines are for testing purpose only
+#def decode():
+#    f2 = open('./public_key.pem', 'r')
+#    bayesian_public_key = f2.read()
+#    try:
+#        string = jwt.decode(token, key=bayesian_public_key, algorithm='RS256')
+#    except Exception as e:
+#        print (e)
+#    print (string)
+#
+#decode()
