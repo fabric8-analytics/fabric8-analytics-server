1abc62254c2b8d3db6d4fb3413485caf13bca6ad
Michal Srb <michal@redhat.com>
2017-07-13 12:18:11 +0200

    Sync update_selinon.sh script with worker

--- hack/update_selinon.sh
+++ hack/update_selinon.sh
@@ -5,9 +5,11 @@
 
 set -e
 
-SELINON_COMMIT=1b6b34a
+SELINON_COMMIT=356cfa6
 SELINONLIB_COMMIT=67d9147
 
 
-pip3 install -U git+https://github.com/selinon/selinon@${SELINON_COMMIT} &&
-  pip3 install -U git+https://github.com/selinon/selinonlib@${SELINONLIB_COMMIT}
+pip3 uninstall --yes selinon selinonlib
+pip3 install git+https://github.com/selinon/selinonlib@${SELINONLIB_COMMIT}
+pip3 install git+https://github.com/selinon/selinon@${SELINON_COMMIT}
+
