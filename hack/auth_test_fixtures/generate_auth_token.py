6c112f678dd53d9360a3397c70b49ad8a43fc39e
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-09-15 09:31:08 +0200

    More code style fixes (minor)

--- hack/auth_test_fixtures/generate_auth_token.py
+++ hack/auth_test_fixtures/generate_auth_token.py
@@ -20,18 +20,18 @@ try:
         'sub': userid
     }
     token = jwt.encode(payload, key=bayesian_private_key, algorithm='RS256')
-    print (token.decode('utf-8'))
+    print(token.decode('utf-8'))
 except Exception as e:
     print(e)
 
-# Following lines are for testing purpose only
-#def decode():
-#    f2 = open('./public_key.pem', 'r')
-#    bayesian_public_key = f2.read()
-#    try:
-#        string = jwt.decode(token, key=bayesian_public_key, algorithm='RS256')
-#    except Exception as e:
-#        print (e)
-#    print (string)
+#  Following lines are for testing purpose only
+# def decode():
+#     f2 = open('./public_key.pem', 'r')
+#     bayesian_public_key = f2.read()
+#     try:
+#         string = jwt.decode(token, key=bayesian_public_key, algorithm='RS256')
+#     except Exception as e:
+#         print (e)
+#     print (string)
 #
-#decode()
+# decode()
