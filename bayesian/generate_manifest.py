c30aff04248fdcc1e271c3aa9fb838afd694285a
rratnawa <rratnawa@localhost.localdomain>
2017-10-13 16:19:41 +0530

    change xml_file method return string instead of file object.

--- bayesian/generate_manifest.py
+++ bayesian/generate_manifest.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from lxml.etree import SubElement, Element, ElementTree
-from io import BytesIO
+from lxml.etree import SubElement, Element, ElementTree, tostring
 
 
 class PomXMLTemplate:
 
-    def __init__(self, json_data=None):
-        if json_data:
-            self._data = dict(json_data)
-        else:
-            raise ValueError
+    def __init__(self, json_data):
+        self._data = json_data
         self.root = Element(
             'project',
             xmlns="http://maven.apache.org/POM/4.0.0",
@@ -20,20 +16,16 @@ class PomXMLTemplate:
         self.create()
 
     def create(self):
-        self._project = self._data.get('project', None)
+        self._project = self._data.get('project')
         if self._project:
-            self._options = self._project.get('options', None)
+            self._options = self._project.get('options')
             SubElement(self.root, 'modelVersion').text = '4.0.0'
             if self._options:
-                SubElement(self.root, 'groupId').text = self._options.get(
-                    'group', None)
-                SubElement(self.root, 'artifactId').text = self._options.get(
-                    'artifactId', None)
-                SubElement(self.root, 'version').text = self._options.get(
-                    'version', None)
+                SubElement(self.root, 'groupId').text = self._options.get('group')
+                SubElement(self.root, 'artifactId').text = self._options.get('artifactId')
+                SubElement(self.root, 'version').text = self._options.get('version')
             SubElement(self.root, 'packaging').text = 'pom'
-            SubElement(self.root, 'description').text = self._project.get(
-                'description', None)
+            SubElement(self.root, 'description').text = self._project.get('description')
             SubElement(self.root, 'url').text = 'https://example.com'
             self.licenses = SubElement(self.root, 'licenses')
             self.license = SubElement(self.licenses, 'license')
@@ -41,13 +33,13 @@ class PomXMLTemplate:
                 self.license, 'name').text = "Apache License, Version 2.0"
             SubElement(
                 self.license, 'url').text = "http://www.apache.org/licenses"
-            self.add_framework(self._data.get('framework', None))
-            self.add_dependencies(self._data.get('dependencies', None))
+            self.add_framework(self._data.get('framework'))
+            self.add_dependencies(self._data.get('dependencies'))
 
     def add_framework(self, fw):
         frameworks = {'springboot': ["org.springframework.boot",
                                      "spring-boot-starter-parent",
-                                     self._data.get('version', None)],
+                                     self._data.get('version')],
                       'wildfly': (None, None, None),
                       'vertx': (None, None, None)
                       }
@@ -68,8 +60,5 @@ class PomXMLTemplate:
                     SubElement(dp, child).text = data
 
     def xml_file(self):
-        tempFile = BytesIO()
-        self.tree.write(tempFile, encoding='utf-8',
+        return tostring(self.root, encoding='utf-8',
                         xml_declaration=True, pretty_print=True)
-        tempFile.seek(0)
-        return tempFile
