17b1a359ffdbfb89a4df75de045e3be840eb56ed
Jyasveer Gotta <jyasveer@gmail.com>
2017-10-19 16:29:00 +0530

    fix review comments

--- tests/test_api_v1.py
+++ tests/test_api_v1.py
@@ -128,6 +128,7 @@ class TestApiV1Root(object):
             "/api/v1/stack-analyses/<external_request_id>",
             "/api/v1/system/version",
             "/api/v1/user-feedback",
+            "/api/v1/user-intent",
             "/api/v1/user-intent/<user>/<ecosystem>"
         ]
     }
