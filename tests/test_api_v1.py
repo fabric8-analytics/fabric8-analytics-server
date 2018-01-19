de864626f5bab947214b4f5a0bb04d6ffafec32d
Mitesh Patel <mitpatel@redhat.com>
2018-01-19 23:03:15 +0530

    Revert "Add Submit feedback class"

--- tests/test_api_v1.py
+++ tests/test_api_v1.py
@@ -140,7 +140,6 @@ class TestApiV1Root(object):
             "/api/v1/set-tags",
             "/api/v1/stack-analyses",
             "/api/v1/stack-analyses/<external_request_id>",
-            "/api/v1/submit-feedback",
             "/api/v1/system/version",
             "/api/v1/user-feedback",
             "/api/v1/user-intent",
