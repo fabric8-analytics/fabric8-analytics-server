77b68d595f77f0292ed072742e24f8e66d8828ff
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-02-16 18:19:49 +0100

    Run tests on CI

--- hack/exec_tests.sh
+++ hack/exec_tests.sh
@@ -2,4 +2,6 @@
 
 set -e
 
-py.test -p no:cacheprovider -vv $@
+# we need no:cacheprovider, otherwise pytest will try to write to directory .cache which is in /usr under unprivileged
+# user and will cause exception
+py.test -p no:cacheprovider --cov=/bayesian/bayesian/ --cov-report term-missing -vv $@
