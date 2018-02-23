a5388c0c435392268ed2ad7ba82699c267dc4567
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-02-23 13:16:15 +0100

    Linter report improvement

--- run-linter.sh
+++ run-linter.sh
@@ -1,5 +1,6 @@
 directories="bayesian hack tests alembic"
 separate_files="setup.py"
+pass=0
 fail=0
 
 function prepare_venv() {
@@ -32,6 +33,7 @@ do
         if [ $? -eq 0 ]
         then
             echo "    Pass"
+            let "pass++"
         else
             echo "    Fail"
             let "fail++"
@@ -54,6 +56,7 @@ do
     if [ $? -eq 0 ]
     then
         echo "    Pass"
+        let "pass++"
     else
         echo "    Fail"
         let "fail++"
@@ -63,8 +66,9 @@ done
 
 if [ $fail -eq 0 ]
 then
-    echo "All checks passed"
+    echo "All checks passed for $pass source files"
 else
-    echo "Linter fail, $fail source files need to be fixed"
+    let total=$pass+$fail
+    echo "Linter fail, $fail source files out of $total source files need to be fixed"
     exit 1
 fi
