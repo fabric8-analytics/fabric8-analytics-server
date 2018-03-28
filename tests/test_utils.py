4d3d18430f0523b4c46d190e742999343496a391
sara-02 <sarahmasud02@gamil.com>
2018-03-28 19:46:29 +0530

    Update convert_semantic_version and select latest version

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,8 +2,13 @@
 
 import datetime
 import pytest
+import semantic_version as sv
 
-from bayesian.utils import do_projection, fetch_file_from_github
+from bayesian.utils import (
+    do_projection,
+    fetch_file_from_github,
+    convert_version_to_proper_semantic as cvs,
+    version_info_tuple as vt)
 from f8a_worker.enums import EcosystemBackend
 from f8a_worker.models import Analysis, Ecosystem, Package, Version, WorkerResult
 from urllib.request import urlopen
@@ -129,3 +134,42 @@ class TestFetchFileFromGithub:
             self.__url, file_name, branch=branch_name)
         assert not bool(
             {'filename', 'filepath', 'content'}.symmetric_difference(result[0].keys()))
+
+
+def test_semantic_versioning():
+    """Check the function cvs()."""
+    version = "-1"
+    assert cvs(version) == sv.Version("0.0.0")
+    version = ""
+    assert cvs(version) == sv.Version("0.0.0")
+    version = None
+    assert cvs(version) == sv.Version("0.0.0")
+    version = "1.5.2.RELEASE"
+    assert cvs(version) == sv.Version("1.5.2+RELEASE")
+    version = "1.5-2.RELEASE"
+    assert cvs(version) == sv.Version("1.5.2+RELEASE")
+    version = "2"
+    assert cvs(version) == sv.Version("2.0.0")
+    version = "2.3"
+    assert cvs(version) == sv.Version("2.3.0")
+    version = "2.0.rc1"
+    assert cvs(version) == sv.Version("2.0.0+rc1")
+
+
+def test_version_info_tuple():
+    """Check the function vt()."""
+    version_str = "2.0.rc1"
+    version_obj = cvs(version_str)
+    version_info = vt(version_obj)
+    assert len(version_info) == 4
+    assert version_info[0] == version_obj.major
+    assert version_info[1] == version_obj.minor
+    assert version_info[2] == version_obj.patch
+    assert version_info[3] == version_obj.build
+    version_obj = ""
+    version_info = vt(version_obj)
+    assert len(version_info) == 4
+    assert version_info[0] == 0
+    assert version_info[1] == 0
+    assert version_info[2] == 0
+    assert version_info[3] == tuple()
