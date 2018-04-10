f35018aa11ed953386ac3af499a73e6a289ea976
Samuzzal Choudhury <samuzzal@redhat.com>
2018-04-10 13:03:04 +0530

    Retrieve pre-generated effective POM from S3

--- bayesian/dependency_finder.py
+++ bayesian/dependency_finder.py
@@ -8,13 +8,15 @@ from sqlalchemy.exc import SQLAlchemyError
 from f8a_worker.manifests import get_manifest_descriptor_by_filename
 from f8a_worker.models import StackAnalysisRequest, Ecosystem
 from f8a_worker.solver import get_ecosystem_solver
+from f8a_worker.storages import AmazonS3
 from tempfile import TemporaryDirectory
 from f8a_worker.workers.mercator import MercatorTask
 
 from .utils import generate_content_hash
 
-
-pom_hash = {}
+_ANALYTICS_BUCKET_NAME = 'boosters-manifest'
+_S3 = AmazonS3(bucket_name=_ANALYTICS_BUCKET_NAME)
+_S3.connect()
 
 
 class DependencyFinder():
@@ -51,75 +53,68 @@ class DependencyFinder():
         # If we receive a manifest file we need to save it first
         result = []
         for manifest in manifests:
-            resolve_poms = False
-            hashed_data = None
-            # Special case where maintaining the results of the booster dependencies in memory.
+            content_hash = None
             if source == 'osio':
                 content_hash = generate_content_hash(manifest['content'])
-                if pom_hash.get(content_hash) is not None:
-                    hashed_data = pom_hash.get(content_hash)
-                else:
-                    resolve_poms = True
+                print ("#################### {} file digest is {}".format(manifest['filename'], content_hash))
+                manifest['content'] = _S3.retrieve_blob(content_hash).decode('utf-8')
 
-            if hashed_data is None:
-                with TemporaryDirectory() as temp_path:
-                    with open(os.path.join(temp_path, manifest['filename']), 'a+') as fd:
-                        fd.write(manifest['content'])
+            with TemporaryDirectory() as temp_path:
+                with open(os.path.join(temp_path, manifest['filename']), 'a+') as fd:
+                    fd.write(manifest['content'])
 
-                    # mercator-go does not work if there is no package.json
-                    if 'shrinkwrap' in manifest['filename'].lower():
-                        with open(os.path.join(temp_path, 'package.json'), 'w') as f:
-                            f.write(json.dumps({}))
+                # mercator-go does not work if there is no package.json
+                if 'shrinkwrap' in manifest['filename'].lower():
+                    with open(os.path.join(temp_path, 'package.json'), 'w') as f:
+                        f.write(json.dumps({}))
 
-                    # Create instance manually since stack analysis is not handled by dispatcher
-                    subtask = MercatorTask.create_test_instance(task_name='metadata')
-                    arguments['ecosystem'] = manifest['ecosystem']
-                    out = subtask.run_mercator(arguments, temp_path, resolve_poms=resolve_poms)
+                # Create instance manually since stack analysis is not handled by dispatcher
+                subtask = MercatorTask.create_test_instance(task_name='metadata')
+                arguments['ecosystem'] = manifest['ecosystem']
+                out = subtask.run_mercator(arguments, temp_path, resolve_poms=False)
 
-                if not out["details"]:
-                    raise FatalTaskError("No metadata found processing manifest file '{}'"
-                                         .format(manifest['filename']))
+            if not out["details"]:
+                raise FatalTaskError("No metadata found processing manifest file '{}'"
+                                     .format(manifest['filename']))
 
-                if 'dependencies' not in out['details'][0] and out.get('status', None) == 'success':
-                    raise FatalTaskError("Dependencies could not be resolved from manifest file"
-                                         "'{}'".format(manifest['filename']))
+            if 'dependencies' not in out['details'][0] and out.get('status', None) == 'success':
+                raise FatalTaskError("Dependencies could not be resolved from manifest file '{}'"
+                                     .format(manifest['filename']))
 
-                out["details"][0]['manifest_file'] = manifest['filename']
-                out["details"][0]['ecosystem'] = manifest['ecosystem']
-                out["details"][0]['manifest_file_path'] = manifest.get('filepath',
-                                                                       'File path not available')
+            out["details"][0]['manifest_file'] = manifest['filename']
+            out["details"][0]['ecosystem'] = manifest['ecosystem']
+            out["details"][0]['manifest_file_path'] = manifest.get('filepath',
+                                                                   'File path not available')
 
-                # Convert dependency specifications for external requests.
-                manifest_descriptor = get_manifest_descriptor_by_filename(manifest['filename'])
-                if 'external_request_id' in arguments:
-                    manifest_dependencies = []
-                    if manifest_descriptor.has_resolved_deps:  # npm-shrinkwrap.json, pom.xml
-                        if "_dependency_tree_lock" in out["details"][0]:  # npm-shrinkwrap.json
-                            if 'dependencies' in out['details'][0]["_dependency_tree_lock"]:
-                                manifest_dependencies = out["details"][0]["_dependency_tree_lock"]\
-                                    .get("dependencies", [])
-                        else:  # pom.xml
-                            if 'dependencies' in out['details'][0]:
-                                manifest_dependencies = out["details"][0].get("dependencies", [])
-                        if manifest_descriptor.has_recursive_deps:  # npm-shrinkwrap.json
-                            def _flatten(deps, collect):
-                                for dep in deps:
-                                    collect.append({'package': dep['name'],
-                                                    'version': dep['version']})
-                                    _flatten(dep['dependencies'], collect)
-                            resolved_deps = []
-                            _flatten(manifest_dependencies, resolved_deps)
-                        else:  # pom.xml
-                            resolved_deps =\
-                                [{'package': x.split(' ')[0], 'version': x.split(' ')[1]}
-                                 for x in manifest_dependencies]
-                    else:  # package.json, requirements.txt
-                        resolved_deps = self._handle_external_deps(
-                            Ecosystem.by_name(db, arguments['ecosystem']),
-                            out["details"][0]["dependencies"])
-                    out["details"][0]['_resolved'] = resolved_deps
-                result.append(out)
-            else:
-                result.append(hashed_data)
+            # If we're handling an external request we need to convert dependency specifications to
+            # concrete versions that we can query later on in the `AggregatorTask`
+            manifest_descriptor = get_manifest_descriptor_by_filename(manifest['filename'])
+            if 'external_request_id' in arguments:
+                manifest_dependencies = []
+                if manifest_descriptor.has_resolved_deps:  # npm-shrinkwrap.json, pom.xml
+                    if "_dependency_tree_lock" in out["details"][0]:  # npm-shrinkwrap.json
+                        if 'dependencies' in out['details'][0]["_dependency_tree_lock"]:
+                            manifest_dependencies = out["details"][0]["_dependency_tree_lock"].get(
+                                "dependencies", [])
+                    else:  # pom.xml
+                        if 'dependencies' in out['details'][0]:
+                            manifest_dependencies = out["details"][0].get("dependencies", [])
+                    if manifest_descriptor.has_recursive_deps:  # npm-shrinkwrap.json
+                        def _flatten(deps, collect):
+                            for dep in deps:
+                                collect.append({'package': dep['name'], 'version': dep['version']})
+                                _flatten(dep['dependencies'], collect)
+                        resolved_deps = []
+                        _flatten(manifest_dependencies, resolved_deps)
+                    else:  # pom.xml
+                        resolved_deps =\
+                            [{'package': x.split(' ')[0], 'version': x.split(' ')[1]}
+                             for x in manifest_dependencies]
+                else:  # package.json, requirements.txt
+                    resolved_deps = self._handle_external_deps(
+                        Ecosystem.by_name(db, arguments['ecosystem']),
+                        out["details"][0]["dependencies"])
+                out["details"][0]['_resolved'] = resolved_deps
+            result.append(out)
 
         return {'result': result}
