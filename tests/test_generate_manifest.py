545057cfcecec4f2a7a76e6d3de2fcd766d57b9e
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-04-12 09:10:15 +0200

    Rest of unit tests for the generate_manifest module

--- tests/test_generate_manifest.py
+++ tests/test_generate_manifest.py
@@ -35,6 +35,70 @@ def test_constructor():
     assert obj is not None
 
 
+def test_attributes():
+    """Test the content of PomXMLTemplate attributes."""
+    obj = PomXMLTemplate({})
+    assert obj._data == {}
+    assert obj.root is not None
+    assert obj.tree is not None
+
+
+def test_add_framework():
+    """Test the method add_framework()."""
+    # should not fail
+    obj = PomXMLTemplate({})
+    obj.add_framework("")
+
+    # should not fail
+    obj = PomXMLTemplate({})
+    obj.add_framework("xyzzy")
+
+    obj = PomXMLTemplate({})
+    obj.add_framework("springboot")
+    assert obj._parent is not None
+
+    obj = PomXMLTemplate({})
+    obj.add_framework("vertx")
+    assert obj._parent is not None
+
+    obj = PomXMLTemplate({})
+    obj.add_framework("wildfly")
+    assert obj._parent is not None
+
+
+def test_xml_string():
+    """Test the method xml_string()."""
+    payload = {
+        "framework": "framework",
+        "dependencies": "deps",
+        "project": {
+            "decription": "desc",
+            "options": {
+                "group": "grp",
+                "artifactId": "aid",
+                "version": "1.0.0"
+            }
+        }
+    }
+    obj = PomXMLTemplate(payload)
+    result = obj.xml_string()
+    assert result is not None
+
+
+def test_add_dependencies():
+    """Test the method add_dependencies()."""
+    obj = PomXMLTemplate({})
+    obj.add_framework("springboot")
+    assert obj._parent is not None
+
+    # should not fail
+    obj.add_dependencies(None)
+    obj.add_dependencies(["dependency1", "dependency2"])
+
+
 if __name__ == '__main__':
     test_constructor()
-    pass
+    test_attributes()
+    test_add_framework()
+    test_add_dependencies()
+    test_xml_string()
