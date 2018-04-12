557f0ad10855dc221b108090f3f2bd65e20332c9
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-04-12 08:47:03 +0200

    Constructor test

--- tests/test_generate_manifest.py
+++ tests/test_generate_manifest.py
@@ -3,5 +3,38 @@
 import pytest
 from bayesian.generate_manifest import *
 
+
+def test_constructor():
+    """Test the constructor for the PomXMLTemplate class."""
+    obj = PomXMLTemplate({})
+    assert obj is not None
+
+    payload = {
+        "framework": "framework",
+        "dependencies": "deps",
+        "project": {
+            "decription": "desc"
+        }
+    }
+    obj = PomXMLTemplate(payload)
+    assert obj is not None
+
+    payload = {
+        "framework": "framework",
+        "dependencies": "deps",
+        "project": {
+            "decription": "desc",
+            "options": {
+                "group": "grp",
+                "artifactId": "aid",
+                "version": "1.0.0"
+            }
+        }
+    }
+    obj = PomXMLTemplate(payload)
+    assert obj is not None
+
+
 if __name__ == '__main__':
+    test_constructor()
     pass
