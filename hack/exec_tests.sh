84cd327ec15839a76d7d63f974f9a22402ea8f52
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-04-27 10:36:58 +0200

    Run CC and MI on CI

--- hack/exec_tests.sh
+++ hack/exec_tests.sh
@@ -2,6 +2,20 @@
 
 set -e
 
+echo "*****************************************"
+echo "*** Cyclomatic complexity measurement ***"
+echo "*****************************************"
+radon cc -s -a -i venv /bayesian/bayesian/ $@
+
+echo "*****************************************"
+echo "*** Maintainability Index measurement ***"
+echo "*****************************************"
+radon mi -s -i venv /bayesian/bayesian/ $@
+
+echo "*****************************************"
+echo "*** Unit tests ***"
+echo "*****************************************"
+
 # we need no:cacheprovider, otherwise pytest will try to write to directory .cache which is in /usr under unprivileged
 # user and will cause exception
 py.test -p no:cacheprovider --cov=/bayesian/bayesian/ --cov-report term-missing -vv $@
