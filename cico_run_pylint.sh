9bcc999cd57abbec26d731d113c36a008fd86d37
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-07 09:24:53 +0100

    Delete env-toolkit before pylint checks

--- cico_run_pylint.sh
+++ cico_run_pylint.sh
@@ -8,6 +8,9 @@ prep() {
     yum -y install python34 python34-virtualenv which
 }
 
+# this script is copied by CI, we don't need it
+rm -f env-toolkit
+
 prep
 ./detect-common-errors.sh
 ./detect-dead-code.sh
