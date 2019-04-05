d6703503ac83a569cc9962aa82c4299498ad75e2
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-04-05 08:10:15 +0200

    Fixed measure-cyclomatic-complexity.sh

--- measure-cyclomatic-complexity.sh
+++ measure-cyclomatic-complexity.sh
@@ -6,8 +6,12 @@ function prepare_venv() {
         # python34 which is in CentOS does not have virtualenv binary
         VIRTUALENV=$(which virtualenv-3)
     fi
-
-    ${VIRTUALENV} -p python3 venv && source venv/bin/activate && python3 "$(which pip3)" install radon==3.0.1
+    if [ $? -eq 1 ]; then
+        # still don't have virtual environment -> use python3.4 directly
+        python3.4 -m venv venv && source venv/bin/activate && python3 "$(which pip3)" install radon==3.0.1
+    else
+        ${VIRTUALENV} -p python3 venv && source venv/bin/activate && python3 "$(which pip3)" install radon==3.0.1
+    fi
 }
 
 [ "$NOVENV" == "1" ] || prepare_venv || exit 1
