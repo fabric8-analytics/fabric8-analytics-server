552c73e15bd1876676c586873ea984498862266e
Yusuf Zainee <yzainee@localhost.localdomain>
2019-05-15 15:09:41 +0530

    typo

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -64,11 +64,26 @@ mocker_input = {
 }
 
 
-ingestion_mocked = {
-    "id": "111",
-    "submitted_at": "10-10-2019",
-    "status": "Submitted"
-}
+def mock_ingestion_response(*_args, **_kwargs):
+    """Mock the call to the insights service."""
+    class MockResponse:
+        """Mock response object."""
+
+        def __init__(self, json_data, status_code):
+            """Create a mock json response."""
+            self.json_data = json_data
+            self.status_code = status_code
+
+        def json(self):
+            """Get the mock json response."""
+            return self.json_data
+    resp = {
+        "id": "111",
+        "submitted_at": "10-10-2019",
+        "status": "Submitted"
+    }
+
+    return MockResponse(resp, 200)
 
 
 @pytest.fixture
@@ -447,28 +462,6 @@ def test_get_ecosystem_from_manifest():
     assert resp is None
 
 
-def mock_ingestion_response(*_args, **_kwargs):
-    """Mock the call to the insights service."""
-    class MockResponse:
-        """Mock response object."""
-
-        def __init__(self, json_data, status_code):
-            """Create a mock json response."""
-            self.json_data = json_data
-            self.status_code = status_code
-
-        def json(self):
-            """Get the mock json response."""
-            return self.json_data
-    resp = {
-        "id": "111",
-        "submitted_at": "10-10-2019",
-        "status": "Submitted"
-    }
-
-    return MockResponse(resp, 200)
-
-
 @patch('bayesian.utils.post', side_effect=mock_ingestion_response)
 def test_server_run_flow(_mocker):
     """Test server_run_flow function."""
@@ -481,9 +474,6 @@ def test_server_run_flow(_mocker):
     }
     res = server_run_flow("someFlow", tmp)
     assert res == "111"
-
-
-
 
 
 def test_check_for_accepted_ecosystem():
