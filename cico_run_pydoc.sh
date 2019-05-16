293b73acc2a798792e58c71e3878a77af648e3a6
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-16 08:42:53 +0200

    Check Python version in Pydoc CI job

--- cico_run_pydoc.sh
+++ cico_run_pydoc.sh
@@ -8,5 +8,10 @@ prep() {
     yum -y install python36 python36-virtualenv which
 }
 
+check_python_version() {
+    python3 tools/check_python_version.py 3 6
+}
+
 prep
+check_python_version
 ./check-docstyle.sh
