219fcad601cec4efe8621367fbc9b9f2585a3ccd
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-16 08:43:14 +0200

    Check Python version in Pylinter CI job

--- cico_run_pylint.sh
+++ cico_run_pylint.sh
@@ -8,10 +8,15 @@ prep() {
     yum -y install python36 python36-virtualenv which
 }
 
+check_python_version() {
+    python3 tools/check_python_version.py 3 6
+}
+
 # this script is copied by CI, we don't need it
 rm -f env-toolkit
 
 prep
+check_python_version
 ./detect-common-errors.sh
 ./detect-dead-code.sh
 ./run-linter.sh
