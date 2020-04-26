66e7d579ac183a6d0abae25f34421658ba9898e2
Dharmendra G Patel <dhpatel@redhat.com>
2020-04-26 16:09:45 +0530

    Corrected indentation error for string continuation(2)

--- bayesian/api_v1.py
+++ bayesian/api_v1.py
@@ -779,9 +779,10 @@ class StackAnalyses(Resource):
                                     ecosystem=ecosystem))
 
         if not check_for_accepted_ecosystem(ecosystem):
-            raise HTTPError(400, error="Error processing request. "
-                                       "'{ecosystem}' ecosystem is not supported".format(
-                                       ecosystem=ecosystem))
+            raise HTTPError(400,
+                            error="Error processing request. "
+                                  "'{ecosystem}' ecosystem is not supported".format(
+                                      ecosystem=ecosystem))
 
         source = request.form.get('source')
         if not (scan_repo_url and ecosystem):
