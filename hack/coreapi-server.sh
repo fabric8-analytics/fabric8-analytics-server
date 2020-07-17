46526e36bd34650210f03c7e424b31821d0df0ec
Dharmendra Gopal Patel <dhpatel@redhat.com>
2020-07-17 17:08:40 +0530

    Modified mod_wsgi error log format

--- hack/coreapi-server.sh
+++ hack/coreapi-server.sh
@@ -4,7 +4,7 @@ set -e
 
 # NOTE: the --python-eggs /home/coreapi is there before I figure out a proper solution
 #   https://github.com/GrahamDumpleton/mod_wsgi/issues/125 or upstream fixes it
-# TIMEOUT params --request-timeout socket-timeout should be added for requests taking longer than expected 
+# TIMEOUT params --request-timeout socket-timeout should be added for requests taking longer than expected
 exec mod_wsgi-express start-server \
                       --entry-point bayesian \
                       --application-type module \
@@ -17,6 +17,7 @@ exec mod_wsgi-express start-server \
                       ${!F8A_DEBUG:---reload-on-changes} \
                       --access-log \
                       --access-log-format "%h %l %u %t \"%r\" %>s %b %{Referer}i \"%{User-agent}i\"" \
+                      --error-log-format "%M" \
                       --log-to-terminal \
                       --python-eggs /home/coreapi \
                       --include-file /etc/httpd/conf.d/coreapi-httpd.conf \
