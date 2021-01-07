0c77bf1475cd41da260bceba385863bbc99bef73
sawood14012 <msawood@redhat.com>
2021-01-07 18:14:10 +0530

    linter fix

--- bayesian/__init__.py
+++ bayesian/__init__.py
@@ -66,9 +66,9 @@ def create_app(configfile=None):
     CORS(app, resources={r"/api/*": {"origins": "*"}})
     CORS(app, resources={r"/user/*": {"origins": "*"}})
 
-    #@app.route('/')
-    #def base_url():
-    ##    return redirect(url_for('api_v1.apiendpoints__slashless'))
+    # @app.route('/')
+    # def base_url():
+    #    return redirect(url_for('api_v1.apiendpoints__slashless'))
 
     setup_logging(app)
 
