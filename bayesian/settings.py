b034f6aeebd858abf20ff4abc9bfeb35c2e257ee
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2021-01-29 18:24:23 +0530

    fix: Increase worker timeout

--- bayesian/settings.py
+++ bayesian/settings.py
@@ -9,7 +9,7 @@ class GunicornSettings(BaseSettings):
 
     workers: int = Field(default=2, env="WORKER_COUNT")
     worker_class: str = Field(default="gevent", env="WORKER_CLASS")
-    timeout: int = Field(default=120, env="WORKER_TIMEOUT")
+    timeout: int = Field(default=300, env="WORKER_TIMEOUT")
     preload: bool = Field(default=True, env="WORKER_PRELOAD")
     worker_connections: int = Field(default=1024, env="WORKER_CONNECTIONS")
 
