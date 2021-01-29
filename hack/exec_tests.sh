018849be1b60e0db3a1ebb75c4f11f2f9de9c3fc
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2021-01-29 17:42:56 +0530

    fix: coverage and image

--- hack/exec_tests.sh
+++ hack/exec_tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # test coverage threshold
-COVERAGE_THRESHOLD=40
+COVERAGE_THRESHOLD=70
 
 check_python_version() {
     python3 /coreapi/tools/check_python_version.py 3 6
@@ -11,7 +11,8 @@ check_python_version
 
 pip3 install -r /coreapi/tests/requirements.txt
 ln -s /coreapi /bayesian
+cd /coreapi
 
 # we need no:cacheprovider, otherwise pytest will try to write to directory .cache which is in /usr under unprivileged
 # user and will cause exception
-py.test -p no:cacheprovider --cov=/coreapi/bayesian/ --cov-report term-missing --cov-fail-under=$COVERAGE_THRESHOLD -vv $@
+py.test -p no:cacheprovider --cov=/coreapi/bayesian/ --cov-report=xml --cov-fail-under=$COVERAGE_THRESHOLD -vv $@
