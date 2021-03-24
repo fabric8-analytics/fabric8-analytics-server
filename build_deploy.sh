150524f3b03b17bc4b99d6408483cc1584fc1db3
Rafiu Jaman Mollah <rmollah@redhat.com>
2021-03-24 16:31:49 +0530

    Added build script.

---
+++ build_deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -exv
+
+BASE_IMG="fabric8-analytics-server"
+QUAY_IMAGE="quay.io/app-sre/${BASE_IMG}"
+IMG="${BASE_IMG}:latest"
+
+GIT_HASH=`git rev-parse --short=7 HEAD`
+
+# build the image
+docker build  --no-cache \
+              --force-rm \
+              -t ${IMG}  \
+              -f ./Dockerfile .
+
+# push the image
+skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
+    "docker-daemon:${IMG}" \
+    "docker://${QUAY_IMAGE}:latest"
+
+skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
+    "docker-daemon:${IMG}" \
+    "docker://${QUAY_IMAGE}:${GIT_HASH}"
