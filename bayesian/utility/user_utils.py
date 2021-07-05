7525154b0166683317956e0dc4628692100b182e
jparsai <jparsai@redhat.com>
2021-07-05 11:59:08 +0530

    User caching. (#769)
    * User details caching mechanism.
    
    * Taking Env variable from settings.
    
    * Doc-Style fixed

--- bayesian/utility/user_utils.py
+++ bayesian/utility/user_utils.py
@@ -10,7 +10,10 @@ from sqlalchemy.dialects.postgresql import insert
 
 from f8a_worker.models import (UserDetails)
 from f8a_utils.user_token_utils import UserStatus
+from bayesian.settings import ENABLE_USER_CACHING, DB_CACHE_DIR
 from bayesian import rdb
+import json
+import os
 
 logger = logging.getLogger(__name__)
 
@@ -52,6 +55,43 @@ def create_or_update_user(user_id, snyk_api_token, user_source):
         rdb.session.rollback()
         logger.exception("Error updating user with id %s", user_id)
         raise UserException("Error updating user") from e
+
+
+def create_or_update_user_in_cache(user_id, snyk_api_token, user_source):
+    """Create or Update User in cache."""
+    if ENABLE_USER_CACHING:
+        db_cache_file_path = DB_CACHE_DIR + "/" + user_id + ".json"
+        user_detail = {"user_id": user_id,
+                       "user_source": user_source,
+                       "snyk_api_token": snyk_api_token,
+                       "status": UserStatus.REGISTERED.name,
+                       "created_date": datetime.datetime.now(),
+                       "registered_date": datetime.datetime.now()}
+        try:
+            with open(db_cache_file_path, 'w', encoding='utf-8') as file:
+                json.dump(user_detail, file, ensure_ascii=False, indent=4, default=str)
+
+            logger.info("User added in cache with id %s", user_id)
+        except Exception as e:
+            logger.exception("Error while caching user with id %s", user_id)
+            raise UserException("Error caching user") from e
+
+
+def get_user_from_cache(user_id):
+    """Get User from cache."""
+    user = {}
+    try:
+        db_cache_file_path = DB_CACHE_DIR + "/" + user_id + ".json"
+
+        if os.path.isfile(db_cache_file_path):
+            logger.info("Found user in cache with id %s", user_id)
+            file = open(db_cache_file_path, "r")
+            user = json.loads(file.read())
+        return user
+    except Exception as e:
+        logger.info("User not found in cache with id %s", user_id)
+        logger.error(e)
+        return None
 
 
 class UserException(Exception):
