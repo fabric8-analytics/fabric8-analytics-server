079954c15a9beeda4bfcaa477308bb10576f4c6a
Rafiu Jaman Mollah <rmollah@redhat.com>
2021-08-09 21:05:23 +0530

    Fixed tests.

--- tests/api/test_api_v2.py
+++ tests/api/test_api_v2.py
@@ -191,8 +191,10 @@ class TestGetTokenApi(unittest.TestCase):
         """Test success get token request."""
         response = self.client.get(api_route_for('/get-token'))
         self.assertEqual(response.status_code, 200)
-        self.assertIn("key", response.json())
-        self.assertIn("url", response.json())
+        if 'key' not in response.json():
+            self.assertTrue(False, "Mssing key in response")
+        if 'url' not in response.json():
+            self.assertTrue(False, "Mssing key in response")
 
     def test_get_token_request_invalid_url(self):
         """Test get token request data with return as 404 error."""
